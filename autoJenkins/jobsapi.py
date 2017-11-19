49a9abf4bd235d820cea64fd1f3820dde58c22ae
yaniv cohen <yanivomc@gmail.com>
2017-11-19 11:46:20 +0200

    job status api created and working

--- autoJenkins/jobsapi.py
+++ autoJenkins/jobsapi.py
@@ -1,5 +1,24 @@
 from autojenkins import Jenkins
-
+import time
 # configure jenkins URL
 j = Jenkins('http://jenkins:8080', auth=('admin', 'admin'))
 
+job = 'FirstPythonJob_unittest'
+# Trigger manual build and get results
+#run job
+j.build(job)
+
+#get last result info
+jobstatus = j.last_result(job)['result']
+x = str(jobstatus)
+
+# Check Job status and do something when completed / failed etc.. 
+while x == "None":
+    print ("Job ", job , "is still building")
+    time.sleep(5)
+    x = j.last_result(job)['result']
+    x = str(x)
+    if x != "None":
+        print ("Job Status is:" , x)
+    else:
+        pass
